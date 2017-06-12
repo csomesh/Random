dd0f05887ab935d19ed710204012a472034eb353
csomesh <c.somesh5@gmail.com>
2017-06-12 11:51:23 +0530

    first commit

--- app/src/main/java/com/example/somesh/random/MainActivity.java
+++ app/src/main/java/com/example/somesh/random/MainActivity.java
@@ -11,34 +11,37 @@ public class MainActivity extends AppCompatActivity {
 
     public int randomInteger(){
         double randomNum = Math.random();
-        randomNum = randomNum*6 + 1;
+        randomNum = randomNum*10 + 1;
         int randomInt = (int) randomNum;
         return randomInt;
     }
+    //int fromInt=0;
+    //int toInt=0;
 
-    EditText from = (EditText) findViewById(R.id.from);
-    EditText to = (EditText) findViewById(R.id.to);
-    Button submit = (Button) findViewById(R.id.submit);
-
-    int fromInt = Integer.parseInt(from.getText().toString());
-    int toInt = Integer.parseInt(to.getText().toString());
-    TextView ranInt = (TextView) findViewById(R.id.ran_int_textview);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+      //  EditText from = (EditText) findViewById(R.id.from);
+        //EditText to = (EditText) findViewById(R.id.to);
+        Button submitButton = (Button) findViewById(R.id.submit);
 
-            submit.setOnClickListener(new View.OnClickListener() {
+
+           submitButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (fromInt > toInt) {
+                    //fromInt = Integer.parseInt(from.getText().toString());
+                    //toInt = Integer.parseInt(to.getText().toString());
+                   /*if (fromInt > toInt) {
                         String warn = "INVALID RANGE";
                         ranInt.setText(warn);
                     } else {
                         ranInt.setText(randomInteger());
-                    }
+                    }*/
+                    TextView ranInt = (TextView) findViewById(R.id.ran_int_textview);
+                    ranInt.setText(String.valueOf(randomInteger()));
                 }
             });
 
